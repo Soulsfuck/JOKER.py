2904d3449a02a800dfa00f504b58d8dad698e0e0
Nox <157261017+Soulsfuck@users.noreply.github.com>
2024-05-14 22:33:39 -0300

    Update joker.py

--- joker.py
+++ joker.py
@@ -1,5 +1,3 @@
-#!usr/bin/python3
-
 import requests
 import threading
 import argparse
@@ -19,32 +17,6 @@ LOGO = """
  #   ##  #    #  #    ##  #    #   
   ####    ####    #####    ####  
 
-███████████████████████████
-███████▀▀▀░░░░░░░▀▀▀███████
-████▀░░░░░░░░░░░░░░░░░▀████
-███│░░░░░░░░░░░░░░░░░░░│███
-██▌│░░░░░░░░░░░░░░░░░░░│▐██
-██░└┐░░░░░░░░░░░░░░░░░┌┘░██
-██░░└┐░░░░░░░░░░░░░░░┌┘░░██
-██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
-██▌░│██████▌░░░▐██████│░▐██
-███░│▐███▀▀░░▄░░▀▀███▌│░███
-██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
-██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
-████▄─┘██▌░░░░░░░▐██└─▄████
-█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
-████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
-█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
-███████▄░░░░░░░░░░░▄███████
-██████████▄▄▄▄▄▄▄██████████
-███████████████████████████
-   
-
-
-
-
-
-
 """
 
 def attack(target, message):
@@ -54,14 +26,23 @@ def attack(target, message):
             print("Request sent to:", target)
         except Exception as e:
             print("Failed to send request:", e)
-        time.sleep(random.uniform(0.5, 2.0))  
+        time.sleep(random.uniform(0.5, 2.0))
 
+
+def clone_repository(stdscr):
+    stdscr.clear()
+    height, width = stdscr.getmaxyx()
+    stdscr.addstr(0, (width // 2) - 15, "Clonando repositório...")
+    stdscr.refresh()
+    subprocess.run(["git", "clone", "--force", "https://github.com/JOJOofSouls/ddos-tool.git"])
+    stdscr.addstr(2, (width // 2) - 15, "Repositório clonado com sucesso! Pressione qualquer tecla para continuar...")
+    stdscr.getch()
 
 def main(stdscr):
     stdscr.clear()
     height, width = stdscr.getmaxyx()
     stdscr.addstr(0, (width // 2) - len(LOGO.split("\n")[0]) // 2, LOGO) 
-    stdscr.addstr(stdscr.getmaxyx()[0] - 2, 0, "Use as setas para navegar e pressione Enter para selecionar uma opção")  
+    stdscr.addstr(height - 3, 0, "Use as setas para navegar e pressione Enter para selecionar uma opção")  
     stdscr.refresh()
 
     options = [
@@ -82,18 +63,15 @@ def main(stdscr):
             current_option += 1
         elif key == curses.KEY_ENTER or key in [10, 13]:  
             if options[current_option][1] == "a":
-               
-                pass
+                pass  # Implemente o início do ataque aqui
             elif options[current_option][1] == "c":
-            
-                pass
+                clone_repository(stdscr)
             elif options[current_option][1] == "i":
-          
-                pass
+                pass  # Implemente a instalação das dependências aqui
             elif options[current_option][1] == "q":
                 break
 
-        stdscr.addstr(stdscr.lines // 2, (stdscr.cols // 2) - len(options[current_option][0]) // 2, options[current_option][0])
+        stdscr.addstr(height // 2, (width // 2) - len(options[current_option][0]) // 2, options[current_option][0])
         stdscr.refresh()
 
 curses.wrapper(main)
